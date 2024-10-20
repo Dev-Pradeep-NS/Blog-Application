44a6be8758de239ca363ec0413bd7438aa1ad860
Dev-Pradeep-NS <nspradeep1501@gmail.com>
2024-10-20 21:34:08 +0530

    Docker customization

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+# #!/bin/sh
 
-# Replace placeholders in env.template with actual env variables
-envsubst < /usr/share/nginx/html/env.template > /usr/share/nginx/html/env.js
+# # Replace placeholders in env.template with actual env variables
+# envsubst < /usr/share/nginx/html/env.template > /usr/share/nginx/html/env.js
 
-# Start Nginx
-nginx -g 'daemon off;'
+# # Start Nginx
+# nginx -g 'daemon off;'
