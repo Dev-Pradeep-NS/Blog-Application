8b9d3a9a177ba47e2ee45f48ac03be06b7636a60
Pradeep <nspradeep53@gmail.com>
2024-10-23 17:02:09 +0530

    Fixes for render

---
+++ start.prod.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Replace placeholders in env.template with actual env variables
+envsubst < /usr/share/nginx/html/env.template > /usr/share/nginx/html/env.js
+
+# Start Nginx
+nginx -g 'daemon off;'
